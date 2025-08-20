052d259f460595efb8668a040404f4d198540ef2
PKdev <152315716+PKMainGit@users.noreply.github.com>
2025-08-20 12:14:20 +0200

    Update
    Updated files:
    - authMiddleware
    - auth
    - reg
    - user

--- src/routes/reg.js
+++ src/routes/reg.js
@@ -4,11 +4,22 @@ import {
   verifyAdmin,
   createUser,
 } from "../controllers/regController.js";
+import {
+  authenticateToken,
+  requireRole,
+  allowIfNoAdmin
+} from "../middlewares/authMiddleware.js";
+
 
 const router = express.Router();
 
 router.get("/check-admin", checkAdminExists);
-router.post("/verify-admin", verifyAdmin);
-router.post("/create-user", createUser);
+router.post("/verify-admin", allowIfNoAdmin, verifyAdmin);
+router.post(
+  "/create-user",
+  authenticateToken,
+  requireRole("admin"),
+  createUser
+);
 
 export default router;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,10 @@
 // src/routes/user.js
 import express from "express";
 import { checkAdminRole } from "../controllers/userController.js";
+import { authenticateToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/check-admin-role", checkAdminRole);
+router.get("/check-admin-role",authenticateToken, checkAdminRole);
 
 export default router;
